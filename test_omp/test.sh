b3e50c68917b61a1c4bb309e5c0b2c2377ab553f
EC2 Default User <ec2-user@ip-172-31-14-19.ec2.internal>
2016-05-11 13:34:48 +0000

    test su S=20

--- test_omp/test.sh
+++ test_omp/test.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-./bfs_omp -V 1 -g 1 -S 20 -E 800 > graph1_S20_E800.txt
-./bfs_omp -V 1 -g 1 -S 27 -E 10 > graph1_S27_E10.txt
+echo 'launching -g1 -S 20 -E 100'
+./bfs_omp -V 1 -g 1 -S 20 -E 100 > graph1_S20_E100.txt
+echo 'done -g1 -S 20 -E 100'
+echo 'launching -g1 -S 20 -E 200'
+./bfs_omp -V 1 -g 1 -S 20 -E 200 > graph1_S20_E200.txt
+echo 'done -g1 -S 20 -E 200'
+echo 'launching -g1 -S 20 -E 400'
+./bfs_omp -V 1 -g 1 -S 20 -E 400 > graph1_S20_E400.txt
+echo 'done -g1 -S 20 -E 400'
+#echo 'launching -g1 -S 20 -E 800'
+#./bfs_omp -V 1 -g 1 -S 20 -E 800 > graph1_S20_E800.txt
+#echo 'done -g1 -S 20 -E 800'
 
+echo 'launching -g2 -S 20 -E 100'
+./bfs_omp -V 1 -g 2 -S 20 -E 100 > graph2_S20_E100.txt
+echo 'done -g2 -S 20 -E 100'
+echo 'launching -g2 -S 20 -E 100'
+./bfs_omp -V 1 -g 2 -S 20 -E 200 > graph2_S20_E200.txt
+echo 'done -g2 -S 20 -E 200'
+echo 'launching -g2 -S 20 -E 400'
+./bfs_omp -V 1 -g 2 -S 20 -E 400 > graph2_S20_E400.txt
+echo 'done -g2 -S 20 -E 400'
+echo 'launching -g2 -S 20 -E 800'
 ./bfs_omp -V 1 -g 2 -S 20 -E 800 > graph2_S20_E800.txt
-./bfs_omp -V 1 -g 2 -S 27 -E 10 > graph2_S27_E10.txt
+echo 'done -g2 -S 20 -E 800'
