b0db159485bc06c1252b9ddcf25082568e641b90
Simone Martucci <lightsaber_91@hotmail.it>
2016-05-26 07:59:07 +0200

    test su cuda effettuati

--- test_cuda/script.sh
+++ test_cuda/script.sh
@@ -8,6 +8,15 @@ echo 'BFS CUDA G1 - S 20 E 150'
 ./bfs_cuda -V 1 -g 1 -S 20 -E 150 > g1_S20_E150
 echo 'BFS CUDA G1 - S 20 E 200'
 ./bfs_cuda -V 1 -g 1 -S 20 -E 200 > g1_S20_E200
+echo 'BFS CUDA G1 - S 20 E 250'
+./bfs_cuda -V 1 -g 1 -S 20 -E 250 > g1_S20_E250
+echo 'BFS CUDA G1 - S 20 E 300'
+./bfs_cuda -V 1 -g 1 -S 20 -E 300 > g1_S20_E300
+echo 'BFS CUDA G1 - S 20 E 350'
+./bfs_cuda -V 1 -g 1 -S 20 -E 350 > g1_S20_E350
+echo 'BFS CUDA G1 - S 20 E 400'
+./bfs_cuda -V 1 -g 1 -S 20 -E 400 > g1_S20_E400
+
 
 echo 'BFS CUDA G2 - S 20 E 50'
 ./bfs_cuda -V 1 -g 2 -S 20 -E 50 > g2_S20_E50
@@ -17,6 +26,14 @@ echo 'BFS CUDA G2 - S 20 E 150'
 ./bfs_cuda -V 1 -g 2 -S 20 -E 150 > g2_S20_E150
 echo 'BFS CUDA G2 - S 20 E 200'
 ./bfs_cuda -V 1 -g 2 -S 20 -E 200 > g2_S20_E200
+echo 'BFS CUDA G2 - S 20 E 250'
+./bfs_cuda -V 1 -g 2 -S 20 -E 250 > g2_S20_E250
+echo 'BFS CUDA G2 - S 20 E 300'
+./bfs_cuda -V 1 -g 2 -S 20 -E 300 > g2_S20_E300
+echo 'BFS CUDA G2 - S 20 E 350'
+./bfs_cuda -V 1 -g 2 -S 20 -E 350 > g2_S20_E350
+echo 'BFS CUDA G2 - S 20 E 400'
+./bfs_cuda -V 1 -g 2 -S 20 -E 400 > g2_S20_E400
 
 echo 'BFS CUDA G1 - S 21 E 20'
 ./bfs_cuda -V 1 -g 1 -S 21 -E 20 > g1_S21_E20
@@ -26,6 +43,8 @@ echo 'BFS CUDA G1 - S 23 E 20'
 ./bfs_cuda -V 1 -g 1 -S 23 -E 20 > g1_S23_E20
 echo 'BFS CUDA G1 - S 24 E 20'
 ./bfs_cuda -V 1 -g 1 -S 24 -E 20 > g1_S24_E20
+echo 'BFS CUDA G1 - S 25 E 20'
+./bfs_cuda -V 1 -g 1 -S 25 -E 20 > g1_S25_E20
 
 echo 'BFS CUDA G2 - S 21 E 20'
 ./bfs_cuda -V 1 -g 2 -S 21 -E 20 > g2_S21_E20
@@ -35,3 +54,5 @@ echo 'BFS CUDA G2 - S 23 E 20'
 ./bfs_cuda -V 1 -g 2 -S 23 -E 20 > g2_S23_E20
 echo 'BFS CUDA G2 - S 24 E 20'
 ./bfs_cuda -V 1 -g 2 -S 24 -E 20 > g2_S24_E20
+echo 'BFS CUDA G2 - S 25 E 20'
+./bfs_cuda -V 1 -g 2 -S 25 -E 20 > g2_S25_E20
